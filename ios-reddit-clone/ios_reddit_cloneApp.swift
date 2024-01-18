1010b374262411601877859056309c81f7948cf7
JahParker <63827876+JahParker@users.noreply.github.com>
2024-01-18 10:39:46 -0500

    Initial Commit

---
+++ ios-reddit-clone/ios_reddit_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_reddit_cloneApp.swift
+//  ios-reddit-clone
+//
+//  Created by Jah're Parker on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_reddit_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
